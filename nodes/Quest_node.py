94ef0df48d8135b9112409a0dea8ef1de446ec4a
GusInfiniteLinesOfCode <101442959+GusInfiniteLinesOfCode@users.noreply.github.com>
2024-02-09 21:42:19 -0500

    added some nodes

---
+++ nodes/Quest_node.py
@@ -0,0 +1,77 @@
+from PySide6 import QtWidgets, QtGui, QtCore
+
+from node_editor.node import Node
+from nodes.common_widgets import TextLineEdit, checkbox
+
+
+class quest_Node(Node):
+    def __init__(self):
+        super().__init__()
+
+        self.title_text = "Quest"
+        self.type_text = "Map"
+        self.set_color(title_color=(0, 128, 0))
+        self.add_pin(name="Input", is_output=False, execution=True)
+        self.add_pin(name="Output", is_output=True, execution=True)
+
+        self.reward_types = ["RANDOM", "LAST", "NEXT"]
+
+        self.build()
+
+    def init_widget(self):
+        self.widget = QtWidgets.QWidget()
+        self.widget.setFixedWidth(150)
+        layout = QtWidgets.QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        self.type_label = QtWidgets.QLabel("Location:")
+        layout.addWidget(self.type_label)
+
+        self.reward_type_dropdown = QtWidgets.QComboBox()
+        self.reward_type_dropdown.addItems(self.reward_types)
+        layout.addWidget(self.reward_type_dropdown)
+
+        #self.amount_label = QtWidgets.QLabel("Amount:")
+        #layout.addWidget(self.amount_label)
+
+        #self.amount_input = QtWidgets.QSpinBox()
+        #self.amount_input.setMinimum(0)
+        #self.amount_input.setMaximum(1000)
+        #layout.addWidget(self.amount_input)
+
+        self.widget.setLayout(layout)
+
+        proxy = QtWidgets.QGraphicsProxyWidget()
+        proxy.setWidget(self.widget)
+        proxy.setParentItem(self)
+
+        super().init_widget()
+
+    def to_dict(self):
+        node_dict = super().to_dict()
+        node_dict["reward_type"] = self.reward_type_dropdown.currentText()
+        #node_dict["amount"] = self.amount_input.value()
+        return node_dict
+
+    def from_dict(self, node_dict):
+        super().from_dict(node_dict)
+        reward_type = node_dict.get("reward_type", "")
+        if reward_type in self.reward_types:
+            index = self.reward_types.index(reward_type)
+            self.reward_type_dropdown.setCurrentIndex(index)
+
+        #amount = node_dict.get("amount", 0)
+        #self.amount_input.setValue(amount)
+
+    def setinternaldata(self):
+        #self.amount_input.setValue(self.internaldata["amount"])
+        self.reward_type_dropdown.setValue(self.internaldata["index"])
+        
+    def setdata(self):
+        
+        #self.internaldata["amount"] = self.amount_input.value()
+        self.internaldata["index"] = self.reward_type_dropdown.currentIndex()
+
+         
+
+# Usage: self.register_node_class(RewardNode)
---
+++ nodes/giveweapon_node.py
@@ -0,0 +1,61 @@
+from PySide6 import QtWidgets, QtGui, QtCore
+
+from node_editor.node import Node
+from nodes.common_widgets import TextLineEdit, checkbox
+
+
+class giveweapon_Node(Node):
+    def __init__(self):
+        super().__init__()
+
+        self.title_text = "give weapon"
+        self.type_text = "Items"
+        self.set_color(title_color=(0, 128, 0))
+        self.add_pin(name="Input", is_output=False, execution=True)
+        self.add_pin(name="Output", is_output=True, execution=True)
+
+        self.reward_types = ["RANDOM"]
+
+        self.build()
+
+    def init_widget(self):
+        self.widget = QtWidgets.QWidget()
+        self.widget.setFixedWidth(150)
+        layout = QtWidgets.QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        self.type_label = QtWidgets.QLabel("Weapon (do RANDOM for random weapon):")
+        layout.addWidget(self.type_label)
+
+        self.amount_input = TextLineEdit()
+        layout.addWidget(self.amount_input)
+
+        self.widget.setLayout(layout)
+
+        proxy = QtWidgets.QGraphicsProxyWidget()
+        proxy.setWidget(self.widget)
+        proxy.setParentItem(self)
+
+        super().init_widget()
+
+    def to_dict(self):
+        node_dict = super().to_dict()
+        node_dict["amount"] = self.amount_input.value()
+        return node_dict
+
+    def from_dict(self, node_dict):
+        super().from_dict(node_dict)
+        amount = node_dict.get("amount", 0)
+        self.amount_input.setText(amount)
+
+    def setinternaldata(self):
+        self.amount_input.setText(self.internaldata["amount"])
+        
+    def setdata(self):
+        
+        self.internaldata["amount"] = self.amount_input.text()
+
+
+         
+
+# Usage: self.register_node_class(RewardNode)
